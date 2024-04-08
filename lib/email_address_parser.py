35ef83c30c8c09c06b558be1250a8c5d189c7e26
Ian July <ianjuly4@gmail.com>
2024-04-08 11:49:16 -0600

    email parser lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self, email):
+        if isinstance(email, str):
+            self.email = email
+        else:
+            raise Exception("Email is not a string")
+        
+    def parse(self):
+        email_addresses = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', self.email)
+        unique_addresses = list(set(email_addresses))
+        unique_addresses.sort()
+        return unique_addresses
